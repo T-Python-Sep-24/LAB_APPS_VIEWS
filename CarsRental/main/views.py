3c524a65cb12eb19edb9a79d5b1cbae0afd8d3b2
Khalid ALMaghyadi <K95.job@gmail.com>
2024-10-28 02:42:56 +0300

    Add lab for Templates, Resolving urls, and Static files

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -6,19 +6,18 @@ import string
 from django.http import HttpRequest , HttpResponse
 
 def home_view(request:HttpRequest):
-    phrase  = "Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here"
-
-    return HttpResponse(phrase)
+   
+    return render(request , "main/home.html")
 
 def about_view (request:HttpRequest):
     
-    paragraph = "Car rentals are an easy way to get a temporary vehicle.\nYou can choose the type of car you need and rent it for a short or long period. Booking is simple, and you can pick up and drop off the car at convenient locations.\n It’s a cost-effective option without the hassle of maintenance or insurance. "
+    # paragraph = "Car rentals are an easy way to get a temporary vehicle.\nYou can choose the type of car you need and rent it for a short or long period. Booking is simple, and you can pick up and drop off the car at convenient locations.\n It’s a cost-effective option without the hassle of maintenance or insurance. "
 
-    return HttpResponse(paragraph)
+    return render(request ,"main/about.html" )
 
-def password_view (requste : HttpRequest): 
+def password_view (requeste : HttpRequest): 
     passwords = []
     for i in range(10):
         password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) )
         passwords.append(password)
-    return HttpResponse(''.join(passwords))
+    return render(requeste, 'main/make_password.html',context= {'passwords': passwords})
