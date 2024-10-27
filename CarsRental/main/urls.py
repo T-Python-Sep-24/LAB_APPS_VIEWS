1a69804d874e7309b20562e81bd5af04d9062454
mishal <dzl15959@gmail.com>
2024-10-28 01:45:40 +0300

    update

--- CarsRental/main/urls.py
+++ CarsRental/main/urls.py
@@ -4,7 +4,9 @@ from . import views
 main="main"
 
 urlpatterns = [
- path("", views.page_view, name="page_view"),
- path("about/",views.car_view,name="car_view"),
- path("password/generate/",views.generate_password,name="generate_password")
+ path("", views.home_page, name="page_view"),
+ path("car/view",views.car_view,name="car_view"),
+ path("password/generate/",views.generate_password,name="generate_password"),
+ path("available/car",views.available_car,name="available_car"),
+ path("about/us",views.about,name="about_us")
  ]
--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -5,12 +5,18 @@ import string
 
 
 # Create your views here.
-def page_view(request:HttpRequest):
-    return HttpResponse("<h1>Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.</h1>")
+def home_page(request:HttpRequest):
+    date="2024-28-10"
+    return render(request,"main/home_page.html",context={"date_time":date})
+
+
+
+def about(request:HttpRequest):
+    return render (request,"main/about.html")
 
 
 def car_view(request:HttpRequest):
-    return HttpResponse("Car rentals allow people to borrow cars for a short time, perfect for travel or temporary use. Customers can choose from different types of cars and pay only for the days they need. This is a flexible and easy way to have a car without owning one.")
+    return render(request,"main/car_view.html")
 
 
 
@@ -18,3 +24,18 @@ def generate_password(request):
     characters = string.ascii_letters + string.digits + "!@#$%^&*"
     password = ''.join(random.choices(characters, k=10))
     return HttpResponse(password)
+
+
+
+def available_car(request:HttpRequest):
+    rate=[
+        {"company":"BMW","model":"2022","status":False},
+        {"company":"Toyota","model":"2024","status":True}
+
+
+
+    ]
+    return render(request,"main/available_car.html",context={"ratte":rate})
+
+
+
