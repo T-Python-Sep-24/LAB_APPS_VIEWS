61801fdc0a27b6267cb90d6af673963588a3d4c0
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-28 00:28:22 +0300

    Done With APPS-VIEWS Lab

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -9,19 +9,12 @@ import string
 
 def main_view(request:HttpRequest):
 
-    return HttpResponse("<h1>Hello World,</h1><h3 style='color:blue;'>This is my new HOME for Car Rentals Website!</h3> We're excited to welcome you here.")
-
+    return render(request, "main/home.html")
 
 
 def about_view(request:HttpRequest):
 
-    return HttpResponse("""
-                        <p style='background-color:powderblue;' 'color:white;'>
-                        Car rentals provide a convenient solution for travelers, offering temporary access to vehicles without the long-term commitment of ownership.
-                        <br>Whether for business trips, vacations, or simply exploring a new city, renting a car allows individuals the freedom to move at their own pace.</br>
-                        With various car types available, customers can choose from economy, luxury, or family-sized vehicles based on their needs and budget.
-                        </p>
-                        """)
+    return render(request, "main/about.html")
 
 
 
@@ -35,4 +28,4 @@ def password_view(request:HttpRequest):
 
     password = ''.join(random.choices(all_characters, k=lenght))
 
-    return HttpResponse(f"Your password is: {password}")
+    return render(request, "main/password_generate.html", context={"password": password})
