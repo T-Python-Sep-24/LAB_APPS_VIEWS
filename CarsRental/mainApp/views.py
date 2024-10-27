605a6856f9977cb07bb96e3e2c2313423bed92c8
Ghaliah <ghalia.banouna@gmail.com>
2024-10-27 15:11:46 +0300

    Second lab work

--- CarsRental/mainApp/views.py
+++ CarsRental/mainApp/views.py
@@ -4,17 +4,17 @@ import secrets, string
 
 #Home page
 def homeView(request: HttpRequest):
-
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website! We're excited to welcome you here.")
+    
+    return render(request, "mainApp/home.html")
 
 #About us page
 def aboutView(request: HttpRequest):
 
-    return HttpResponse("Quickly rent a car in Saudi Arabia with Car Rentals!<br>Explore the kingdom comfortably with our services.")
+    return render(request, "mainApp/about.html")
 
 #Password generator page
 def passwordGenView(request: HttpRequest):
 
     #Generate a password that is 10 characters long
     password = secrets.token_urlsafe(10)
-    return HttpResponse("Your password: " + password)
+    return render(request, "mainApp/passwordGen.html", context={"password" : password})
