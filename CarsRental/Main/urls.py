a5a927ecfe20949d28de41d05c1c4c9a663fa22f
Fahad <fahad.alabdulkarim5@gmail.com>
2024-10-27 14:36:46 +0300

    LAB_APPS_VIEWS_lab2

--- CarsRental/Main/urls.py
+++ CarsRental/Main/urls.py
@@ -4,7 +4,7 @@ app_name = "Main"
 urlpatterns = [
     path("", views.main_view, name="main_view"),
     path("about/", views.about_view, name="about_view"),
-    path('password/generate/', views.password_view, name='generate-password'),
+    path('password/generate/', views.password_view, name='password_view'),
 
 
 ]
--- CarsRental/Main/views.py
+++ CarsRental/Main/views.py
@@ -6,17 +6,16 @@ import string
 
 
 def main_view(request : HttpRequest):
-    content = "Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here."
-    return HttpResponse(content)
+    return render(request, "main/main.html")
+
 
 
 def about_view(request : HttpRequest):
-    content = "Car rentals offer customers the flexibility to rent vehicles for short or long-term use, making travel or commuting convenient without the need for ownership. They provide a range of vehicles, from budget options to premium cars, catering to various needs, such as business trips, vacations, or temporary replacements. Car rental services are accessible through local branches, websites, or apps, allowing for easy booking and quick pickups."
-    return HttpResponse(content)
+    return render(request, "main/about.html")
 
 def password_view(request : HttpRequest):
     length = 10
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(characters) for _ in range(length))
     
-    return HttpResponse(f"Generated Password: {password}")
+    return render(request, "main/password.html", context={"password" : password})
