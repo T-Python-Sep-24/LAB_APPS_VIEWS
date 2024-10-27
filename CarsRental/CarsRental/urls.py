b5a82475f8f13a4c81d3b76a0d705437c1a48dd0
Ali <almaghamsiali@gmail.com>
2024-10-27 13:46:32 +0300

    small change on file

--- CarsRental/CarsRental/urls.py
+++ CarsRental/CarsRental/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
-app_name='main'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls')),
