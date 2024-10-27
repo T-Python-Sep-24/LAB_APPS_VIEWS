a75a544b8d889ae4a96e49945cb59a33d225dba9
alhanoufALLUHAYDDAN <alhanoufkhalaf1@gmail.com>
2024-10-27 19:44:49 +0300

    done: templates / resolving URLs / static files

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -6,18 +6,16 @@ import string
 
 def home_view(request:HttpRequest):
 
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here.")
+    return render(request, "main/home.html")
 
 def about_view(request:HttpRequest):
 
-    return HttpResponse("Car rental agencies primarily serve people who require a temporary vehicle, for example, those who do not own their own car, travelers who are out of town .")
+    return render(request,"main/about.html")
 
 def password_view(request:HttpRequest):
    
-
-
     lenght = 10
 
     all_characters = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
     password = "".join(random.choice(all_characters) for _ in range(lenght))
-    return HttpResponse(password)
+    return render(request, "main/password.html",context={"password" : password})
