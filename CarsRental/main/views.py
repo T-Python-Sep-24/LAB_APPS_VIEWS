f70759b77af2b6b67459b2d0d50994ad4c95c569
Eyad <eyalghamdi@outlook.com>
2024-10-28 11:35:05 +0300

    Done

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -3,12 +3,12 @@ from django.http import HttpRequest, HttpResponse
 import faker
 
 def main_view(request:HttpRequest):
-    return HttpResponse("<h1>Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.</h1>")
+    return render(request,"index.html")
 
 def about_view(request:HttpRequest):
-    return HttpResponse("<p1>A simple paragraph about Car Rentals.</p1>")
+    return render(request,"about.html")
 
 def password_generator_view(request:HttpRequest):
     password = faker.Faker()
     random_password = password.password(10,True,True,True,True)
-    return HttpResponse(random_password)
+    return render(request,"password.html",context={"random_password":random_password})
