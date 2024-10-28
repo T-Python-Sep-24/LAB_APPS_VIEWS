85acbc95e30782b78630c6e739ef74999cb8348b
osama_fahad <osamaf.net@gmail.com>
2024-10-28 11:49:50 +0300

    solved template/static lab

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -5,10 +5,10 @@ import random
 import string
 
 def welcome(request: HttpRequest):
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.")
+    return render(request, "main/welcome.html")
 
 def aboutUs(request: HttpRequest):
-    return HttpResponse("A car rental website is a place where you can find and book cars to use for a short time. You go to the website, choose the car you want, and select the days you need it. It is easy to see pictures of the cars and read about their prices. When you finish, you can pay online and pick up the car at a nearby location. This helps you travel easily without owning a car.")
+    return render(request, "main/about.html")
 
 def passGenerator(request: HttpRequest):
     allChars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
@@ -19,4 +19,4 @@ def passGenerator(request: HttpRequest):
         
     password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + randomChoice + random.choice(string.punctuation)
     
-    return HttpResponse(password)
+    return render(request, "main/password.html", context={"password": password})
