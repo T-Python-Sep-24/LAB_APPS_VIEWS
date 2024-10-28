8f2d70a7b087b9a6f0511e9e9af583293dfcc9a5
Mohamed <sirexplq@gmail.com>
2024-10-28 11:24:23 +0300

    solved

--- CarsRental/app_1/views.py
+++ CarsRental/app_1/views.py
@@ -4,12 +4,13 @@ import random
 import string
 # Create your views here.
 def page_view(request : HttpRequest):
-    content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
-    return HttpResponse(content)
+    return render(request, 'app_1/page_view.html')
+
 def about(request:HttpRequest):
-    return HttpResponse("Car rental is when you rent a car.")
+    return render(request, 'app_1/about.html')
+
 def rand_pass(request:HttpRequest):
     length=10
     characters= string.ascii_letters + string.digits+string.punctuation
-    generate_pass=''.join(random.choice(characters) for _ in range(length))
-    return HttpResponse(generate_pass)
+    password=''.join(random.choice(characters) for _ in range(length))
+    return render(request, 'app_1/rand_pass.html', {'password': password})
