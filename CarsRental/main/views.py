ab1f02cc13b64527bf60cc531f11d0cda918f601
Reema <reemaalbrahim28@gmail.com>
2024-10-28 14:09:11 +0300

    Extra stuff

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -7,20 +7,22 @@ import string
 
 def page_view(request : HttpRequest):
  
-  return HttpResponse("Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.")
+  return render(request,"main\page.html")
 
 
 
 def about_view(request : HttpRequest):
  
-  return HttpResponse("A car rental reservation system is a software solution that helps manage the car rental business, streamlining and automating the majority of operational workflows.")
+  return render(request,"main/about.html")
 
 
 
- 
+
 
 def generate_pass(request):
-    length = 10
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return HttpResponse(password)  
+ length = 10
+ characters = string.ascii_letters + string.digits + string.punctuation
+    
+    # Generate a password
+ password = ''.join(random.choice(characters) for _ in range(length))
+ return render(request,"main\password.html", context={'password':password})
